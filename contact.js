40a514cb8648a80d08c0c4da62d3223f98ff1281
Jeff Heaton <jeff@jeffheaton.com>
2017-07-22 13:24:10 -0600

    Update website from hexo deploy

---
+++ contact.js
@@ -0,0 +1,28 @@
+$("#sendMessage").click("click", function() {
+    var name = $('#inputName').val().trim()
+    var email = $('#inputEmail').val().trim()
+    var message = $('#inputMessage').val().trim()
+    var subject = $('#inputSubject').val().trim()
+
+    if( name.length == 0 || email.length==0 || message.length==0 || subject.length==0 ) {
+      alert("All four fields must be filled in.")
+      return
+    }
+
+    if( email.indexOf('.') ==-1 && email.indexOf("@")==-1 ) {
+      alert("Invalid email.")
+      return
+    }
+
+    $.ajax({
+      url: "//formspree.io/jtheaton@wustl.edu",
+      method: "POST",
+      data: {name: name, email: email, message: message, _subject: subject, _replyto: email},
+      dataType: "json"
+    }).done(function() {
+      alert('Message sent.')
+      window.location = "/"
+    });
+
+
+})
